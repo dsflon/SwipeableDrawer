c01497552b696cc671b1b8fd11a6ff778aa39988
Daiki Saito <dsflon@gmail.com>
2019-09-19 21:10:57 +0900

    修正

--- components/HalfModal.vue
+++ components/HalfModal.vue
@@ -90,7 +90,7 @@ export default {
     touchEnd() {
       this.endY = this.moveY;
 
-      if (this.moveY > 100) {
+      if (this.moveY > 50) {
         this.closeModal();
       } else {
         this.openModal();
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,9 @@
 <template>
   <div :class="$style.container">
-    <button @click="toggleModal">
-      開く
-    </button>
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-    <button @click="toggleModal">
-      開く
-    </button>
+    <template v-for="n in 10">
+      <button :key="n" :class="$style.button" @click="toggleModal" />
+    </template>
+
     <HalfModal :opened="opened" @close="toggleModal">
       aasdfasd aasdsd aasdsd aasdsd aasdsd aasdsd aasdfasdfa aasdfasdfa
       aasdfasdfa aasdfasdfa aasdfasdfa aasdfasdfa aasdfasdfa aasdfasdfa
@@ -41,11 +38,37 @@ export default {
 
 <style module>
 .container {
-  height: 1000px;
-  background: linear-gradient(
-    to bottom,
-    rgba(33, 28, 14, 0) 0%,
-    rgba(33, 28, 14, 0.7) 100%
-  );
+  background: #f3f1ea;
+  padding: 10px;
+}
+
+.button {
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.1);
+  border: none;
+  margin-bottom: 10px;
+  width: 100%;
+  height: calc(20vmin + 20px);
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+}
+
+.button:before {
+  content: '';
+  display: block;
+  width: 20vmin;
+  height: 0;
+  padding-bottom: 20vmin;
+  background: #ddd;
+}
+
+.button:after {
+  content: 'テキストが入ります。テキストが入ります。テキストが入ります。';
+  display: block;
+  text-align: left;
+  width: 66vmin;
 }
 </style>
